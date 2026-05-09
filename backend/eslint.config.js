6c47fbb0304fc2f3fa0b339b52660fdbc77cd529
temmyjerrybooks <temmyjerrybooks@gmail.com>
2026-05-09 16:34:14 +0100

    ci: add ESLint config for backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,27 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        Buffer: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
+];
